08f17c3e3e095d415d919c63a9226b0f1ba88998
lakumnirali <lakumnirali@gmail.com>
2023-07-13 13:39:28 +0530

    slider

--- cakeology2/src/componet/navbar/home.jsx
+++ cakeology2/src/componet/navbar/home.jsx
@@ -1,23 +1,15 @@
-// import React from 'react';
-
-// const Home = () => {
-//     return (
-//         <div>
-//             HOME
-
-//         </div>
-//     );
-// };
-
-// export default Home;
 import React, { Component } from 'react';
-
+import Carousel from './carousel/carousel.jsx'
+import Card from './card/card.jsx'
 class home extends Component {
     render() {
         return (
-            <div>
-              grghh  
-            </div>
+            <>
+              <>
+              <Carousel/>
+              <Card/>
+              </>
+            </>
         );
     }
 }
