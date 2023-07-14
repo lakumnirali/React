4615ff5773225729f494af60b5dc4ee69cbd9754
lakumnirali <lakumnirali@gmail.com>
2023-07-14 15:17:10 +0530

    card

--- cakeology2/src/componet/navbar/carousel/carousel.jsx
+++ cakeology2/src/componet/navbar/carousel/carousel.jsx
@@ -15,7 +15,7 @@ export default class AutoPlay extends Component {
       slidesToScroll: 1,
       autoplay: true,
       speed: 8000,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 5000,
       cssEase: "linear"
     };
     return (
