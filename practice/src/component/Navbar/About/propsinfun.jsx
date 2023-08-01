c7646da2e030736baa27001ba14dcfb8c448a2fb
lakumnirali <lakumnirali@gmail.com>
2023-08-01 13:41:51 +0530

    propsin functional component

--- practice/src/component/Navbar/About/propsinfun.jsx
+++ practice/src/component/Navbar/About/propsinfun.jsx
@@ -3,6 +3,7 @@ import Propschildcard from './propschildcard.jsx'
 import Cimg2 from '../../../cimg2.jpg'
 import PropTypes from 'prop-types';
 
+
 Propsinfun.propTypes = {
 
 };
@@ -14,9 +15,9 @@ function Propsinfun(props) {
                 <div className="col-lg-3">
                     <Propschildcard cardtitle="data" imgdata='https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
                 </div>
-                <div className="col-lg-3"><Propschildcard /></div>
-                <div className="col-lg-3"><Propschildcard /></div>
-                <div className="col-lg-3"><Propschildcard /></div>
+                <div className="col-lg-3"><Propschildcard  imgdata={process.env.PUBLIC_URL + '/image/img1.jpg'} /> </div>
+                <div className="col-lg-3"><Propschildcard imgdata={Cimg2} /></div>
+                <div className="col-lg-3"><Propschildcard imgdata={'/image/img3.jpg'} /></div>
             </div>
         </>
     );
