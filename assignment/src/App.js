f950020e1b1b188281298cb1539b455d79f67c76
lakumnirali <lakumnirali@gmail.com>
2023-08-02 11:18:02 +0530

    data

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+
 import Reactintro from './component/reactintro.jsx'
 import Renderlist from './component/renderlist.jsx'
+import Incrementdecrement from './component/Incrementdecrement.jsx'
+import './app.css'
 class App extends Component {
   render() {
     return (
       <>
       <Reactintro/>
       <Renderlist/>
+      <Incrementdecrement/>
       </>
 
     );
---
+++ assignment/src/component/powerfullwomen.js
@@ -0,0 +1,43 @@
+  export const Powerfullwomen=[{
+     id:0,
+     Name:"Indira Gandhi",
+     Born: "19 November 1917, Prayagraj",
+     Awards: "Bharat Ratna, Lenin Peace Prize, Jawaharlal Nehru Award for International Understanding",
+     ImageUrl:"https://m.media-amazon.com/images/M/MV5BNzQ0ODQzOTQtNmViMi00OWM4LTg4ZjEtZGNkNjczZmYxYTU2XkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg"
+  },
+  {
+    id:1,
+    Name:"Sushma Swaraj",
+    Born: "14 February 1952, Ambala Cantt",
+    Awards: "Padma Vibhushan",
+    ImageUrl:" https://images.livemint.com/rf/Image-621x414/LiveMint/Period2/2016/12/12/Photos/Processed/swarajswaraj-kLCE--621x414@LiveMint.jpg"
+  },
+  {
+id:2,
+Name:"Jayaram Jayalalithaa",
+Born: "24 February 1948, Melukote",
+Awards:"Filmfare Award - Telugu in 1973,Filmfare Award South in 1971",
+ImageUrl:"https://w0.peakpx.com/wallpaper/990/498/HD-wallpaper-j-jayalalitha-painting-art-politician-actress-amma.jpg"
+},
+{
+    id:3,
+    Name:"Mamata Banerjee",
+    Born: "5 January 1955, Kolkata",
+    Awards: "CNN-IBN Indian of the Year in Politics",
+    ImageUrl:"//pbs.twimg.com/profile_images/1586388609388908545/fVBtJGLA_400x400.jpg"
+},
+{
+id:4,
+Name:"Hima Das",
+Born: "9 January 2000 , Nagaon",
+Awards: "Arjuna Award for Athletics",
+ImageUrl:"//im.rediff.com/sports/2018/apr/10hima.jpg?w=670&h=900"
+},
+{
+    id:5,
+    Name:"Mary Kom",
+    Born: "24 November 1982, Kangathei",
+    Awards: "Padma Vibhushan, Padma Bhushan, Padma Shri",
+    ImageUrl:"https://www.deccanherald.com/sites/dh/files/article_images/2020/04/01/PTI10_13_2019_000226B-min-1585731550.jpg"
+}
+]
