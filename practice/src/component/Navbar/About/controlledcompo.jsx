a8f44fead46786107fbf77f596665a29355f3682
lakumnirali <lakumnirali@gmail.com>
2023-08-04 12:04:34 +0530

    control

--- practice/src/component/Navbar/About/controlledcompo.jsx
+++ practice/src/component/Navbar/About/controlledcompo.jsx
@@ -3,21 +3,27 @@ class controlledcompo extends Component {
     constructor(props){
         super(props);
         this.state={
-            name:""
+            name:"",
+            password:""
         }
     }
     setInputData=(event)=>{  
         this.setState({name:event.target.value})
+        // this.setState({password:event.target.value})
     }
     render() {
         return (
             <div>
                 <label >my name is</label>&nbsp;
-                {/* {JSON.stringify(this.state)} */}
             {this.state.name}&nbsp; <br />
                 <input type="text"  onChange={this.setInputData}  id="" /> 
+                <input type="text"  onChange={(e)=>{ this.setState({password:e.target.value}) }} id="" />
+                {JSON.stringify(this.state.password)}
             </div>
         );
     }
 }
 export default controlledcompo;
+
+
+
