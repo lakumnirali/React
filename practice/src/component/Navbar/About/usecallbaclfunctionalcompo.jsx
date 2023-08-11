196386caf61cae2c583fd098632de8ab1a1a625d
lakumnirali <lakumnirali@gmail.com>
2023-08-11 13:05:33 +0530

    usecallbackfunction

---
+++ practice/src/component/Navbar/About/usecallbaclfunctionalcompo.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+const funcount = new Set();
+const usecallbaclfunctionalcompo = () => {
+    const [count, setcount]=useState(0);
+  
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default usecallbaclfunctionalcompo;
