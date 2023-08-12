4d386720230f935b50a4f5482d1ac0c0d9073b60
lakumnirali <lakumnirali@gmail.com>
2023-08-12 12:36:07 +0530

    lifecycleinclasscompo

--- practice/src/component/Navbar/About/lifecycleinclass.jsx
+++ practice/src/component/Navbar/About/lifecycleinclass.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Lifecycleinclass extends Component {
+    constructor(props){
+        console.log("called");
+        super(props);
+        this.state={time:new Date()};
+    }
+   tick=()=>{
+    console.log("called ");
+    this.setstate={
+        time:new Date()
+    };
+   }
+   
     render() {
         return (
             <>
              
+             l
              componentWillMount() {
                 
              }
